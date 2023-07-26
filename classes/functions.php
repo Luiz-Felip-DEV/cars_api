65ecdc12efda9d1bf9ab20b73aee71abfc49d22b
luiz.souza <luiz.martins@unitri.edu.br>
2023-07-26 17:02:14 -0300

    funções auxiliares criadas na functions.php

--- classes/functions.php
+++ classes/functions.php
@@ -16,6 +16,51 @@
             return json_encode($response);
         }
 
+        public function formatedNumber($telefone)
+        {
+            $numeroSemFormatacao    = preg_replace("/[^0-9]/", "", $telefone);
+            $numeroFormatado        = preg_replace("/(\d{2})(\d{5})(\d{4})/", "($1) $2-$3", $numeroSemFormatacao);
+
+            return $numeroFormatado;
+        }
+
+        public function getID($email, $password)
+        {
+            $db = DB::connect();
+            $rs = $db->prepare("SELECT id FROM users WHERE email = '{$email}' AND password = '{$password}'");
+
+            $rs->execute();
+            $obj = $rs->fetchObject();
+
+            return $obj;
+        }
+
+        public function verifyEmail($email)
+        {
+            $db = DB::connect();
+            $rs = $db->prepare("SELECT email FROM users WHERE email = '{$email}'");
+            $rs->execute();
+            $obj = $rs->fetchAll();
+            
+            $resp = (count($obj) !== 0) ? false : true;
+
+            return $resp;
+        }
+
+        public function verifyTelephone($telefone)
+        {
+            $numeroFormatado = $this->formatedNumber($telefone);
+    
+            $db = DB::connect();
+            $rs = $db->prepare("SELECT telephone FROM users WHERE telephone = '{$numeroFormatado}'");
+            $rs->execute();
+            $obj = $rs->fetchAll();
+
+            $resp = (count($obj) !== 0) ? false : true;
+
+            return $resp;
+        }
+
     }
 
 ?>
